fe4a316e82d9d29a3444b470b09a7835c60522d9
DTrejo <david.daniel.trejo@gmail.com>
2018-05-25 17:40:03 -0700

    fix deploy

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/colorpreview/', // for github pages off dtrejo.com/colorpreview/*
 
     /**
      * Source Maps
